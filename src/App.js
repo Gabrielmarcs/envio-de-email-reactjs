efe4c6852bede58264f8a7f4ac7f54d170a9fffd
Gabriel Marcos Lopes <gabriel.lopes3@estudante.ufla.br>
2022-10-03 20:09:31 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import emailjs from '@emailjs/browser';
 
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+function sendEmail(e){
+  e.preventDefault();
+
+  if(name === '' || email === '' || message ==='' ){
+    alert('PREENCHA TODOS OS CAMPOS');
+    return;
+  }
+
+  const templateParams = {
+    from_name: name,
+    message: message,
+    email: email
+  }
+  
+  emailjs.send("service_tcss22o", "template_yem4df7", templateParams, "wgyFEoomybxPGjYxY")
+
+  .then((response) => {
+    console.log("EMAIL ENVIADO", response.status, response.text)
+    
+    setName('')
+    setEmail('')
+    setMessage('')
+  }, (err) => {
+    console.log("ERRO ", err)
+  })
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Contato</h1>
+
+      <form className="form" onSubmit={sendEmail}>
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+
+        <textarea 
+          className="textarea"
+          placeholder="Digite sua mensagem..."
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+
+        <input className="button" type="submit" value="Enviar" />
+      </form>
+
     </div>
   );
 }
